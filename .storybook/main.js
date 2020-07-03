8556487c054b79fed5a12a9a8ea7fc0e8c0e57eb
Roberto Nobre <robertonobre90@gmail.com>
2020-07-02 21:29:17 -0300

    manager preview html page

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../src/**/*.stories.ts'],
+  stories: ['../src/**/0-Welcome.stories', '../src/**/1-Button.stories'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
 };
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,4 +3,8 @@ import Theme from './theme';
 
 addons.setConfig({
   theme: Theme,
+  previewTabs: {
+    // the order of the tabs is configured by the order here
+    canvas: null, // canvas is here simply listed so its place as first tab
+  },
 });
