aab68ac83a777974110e2652dbe862c35443e6ae
Roberto Nobre <robertonobre90@gmail.com>
2020-07-08 23:18:05 -0300

    Brand image changed

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,7 +4,7 @@ export default create({
   colorSecondary: '#396FAC',
   appBorderColor: '#396FAC',
 
-  brandImage: 'https://candidatos.atento.com.br/content/images/candidatos.png',
+  brandImage: 'baseurl',
   brandUrl: 'ok',
   base: 'light'
 });
