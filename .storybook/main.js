34740bc778b08c68f653b575ba7c293f2ac5d351
Roberto Nobre <robertonobre90@gmail.com>
2020-07-16 22:30:22 -0300

    Storybook configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: [
     '../src/**/button.stories.ts',
     '../src/**/input.stories.ts',
+    '../src/**/icon.stories.ts',
   ],
   addons: [
     '@storybook/addon-actions',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './components/input/input.component';
 import { ButtonComponent } from './components/button/button.component';
+import { IconComponent } from './components/icon/icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
-    ButtonComponent
+    ButtonComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularSvgIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
