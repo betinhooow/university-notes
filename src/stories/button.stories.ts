9bf3147c6e74e1c10b1501f06bddc2bfdabe6c85
Roberto Nobre <robertonobre90@gmail.com>
2020-07-16 22:24:19 -0300

    Button stories

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -6,26 +6,32 @@ import { centered } from '@storybook/addon-centered/angular';
 
 //@ts-ignore
 import ButtonReadme from 'src/app/components/button/button.readme.md';
+import { IconComponent } from 'src/app/components/icon/icon.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
 
-storiesOf('Button Component', module)
+storiesOf('Button', module)
 .addDecorator(withA11y)
 .addDecorator(centered)
 .addDecorator(
   moduleMetadata({
     declarations: [
-      ButtonComponent
+      ButtonComponent,
+      IconComponent
     ],
     imports: [
-      BrowserModule
+      BrowserModule,
+      AngularSvgIconModule,
+      HttpClientModule
     ],
     providers: [],
   })
 )
-.add('Default', () => {
-  return {
+.add('Default', () => ({
+
     template: `<app-button>Entrar</app-button>`
-  }
-}, {
+
+}), {
   notes: { markdown: ButtonReadme }
 })
 .add('Disabled', () => {
