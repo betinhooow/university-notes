2ab9c1afeaa614bcac035edc2ef957f784a47367
Roberto Nobre <robertonobre90@gmail.com>
2020-07-29 22:32:40 -0300

    add item icon list

--- src/app/components/icon/icons.js
+++ src/app/components/icon/icons.js
@@ -3,11 +3,13 @@ import Eye from './../../../assets/icons/eye.svg';
 import EyeHide from './../../../assets/icons/eye-hide.svg';
 import Notification from './../../../assets/icons/notification.svg';
 import RemoveRound from './../../../assets/icons/remove-round.svg';
+import AddFull from './../../../assets/icons/add-full.svg';
 
 export default {
   ArrowDown,
   Eye,
   EyeHide,
   Notification,
-  RemoveRound
+  RemoveRound,
+  AddFull
 };
