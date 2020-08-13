6ce05882ba66ec19d923101e4d935fb008c9a15a
Roberto Nobre <robertonobre90@gmail.com>
2020-08-12 22:36:08 -0300

    added new tab story

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,8 @@ module.exports = {
     '../src/**/button.stories.ts',
     '../src/**/input.stories.ts',
     '../src/**/icon.stories.ts',
-    '../src/**/card.stories.ts'
+    '../src/**/card.stories.ts',
+    '../src/**/tab.stories.ts'
   ],
   addons: [
     '@storybook/addon-actions',
